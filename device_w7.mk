1f5d4886ca04b492bd8664391d73d42ac1d7cf77
BigMajster <maszlij11@gmail.com>
2014-11-22 11:14:53 +0100

    Add missing google codecs
    Change-Id: Ifee96fe1f6e9a76263961d43d8fae28949e13b85

--- device_w7.mk
+++ device_w7.mk
@@ -64,6 +64,9 @@ PRODUCT_COPY_FILES += \
     device/lge/w7/prebuilt/etc/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
     device/lge/w7/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
     device/lge/w7/prebuilt/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/lge/w7/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
     device/lge/w7/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
     device/lge/w7/prebuilt/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
