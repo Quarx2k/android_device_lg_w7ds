a7564222b4aeddb9bb70334067903f5cda33a9c6
Quarx2k <agent00791@gmail.com>
2015-08-08 00:03:58 +0600

    Fix build prop

--- cm.mk
+++ cm.mk
@@ -8,5 +8,8 @@ TARGET_SCREEN_HEIGHT := 960
 # Release name
 PRODUCT_RELEASE_NAME := LG L90
 PRODUCT_NAME := cm_w7
+PRODUCT_DEVICE := w7
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := w7
 
 $(call inherit-product, device/lge/w7/full_w7.mk)
