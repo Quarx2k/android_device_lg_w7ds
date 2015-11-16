5473100ff4f42a68c7048a64001d07bfe0f3136c
Nickolay Semendyaev <agent00791@gmail.com>
2015-11-17 03:47:50 +0600

    Add missing timkeep packages

--- device_w7.mk
+++ device_w7.mk
@@ -277,6 +277,11 @@ PRODUCT_PACKAGES += \
     hostapd_default.conf \
     libnetcmdiface
 
+# Sony timekeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bt.le_dev_pwr_class=1 \
